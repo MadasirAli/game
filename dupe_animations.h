5de9ef5fe88d56d3ed6bec0d92033d94ff649fdf
Madasir Ali <madasirali672@gmail.com>
2025-03-19 16:59:10 +0500

    easers and interpolators added, linear easer, quad_in, quad_out, quad_in_out
    easing functionality in animations added
    
    but the interpolations are between points,,,,,,,,,,,,,,,
    so it the interpolations will work for only 2 points,
    
    moving easing types to per point,

--- dupe_animations.h
+++ dupe_animations.h
@@ -5,17 +5,21 @@
 
 namespace game {
   struct dupe_animations {
-    const anim::anim_clip<3, 100> headIdle {
-      true,
-      false,
-      false,
-      true,
-      true,
-      2.0f,
+    const anim::anim_clip<2, 100> headIdle {
+      true,         // allow pos
+      false,        // allow rot
+      false,        // allow scale
+      true,         // allow sprite
+      true,         // loop
+      2.0f,         // duration
+      anim::ease::quad_in_out,   // pos ease
+      anim::ease::linear,   // rot ease
+      anim::ease::linear,   // scale ease
       {
-        anim::anim_point(0, 0.0f, 0.0f, 0.0f, 0, 0, 0),
-        anim::anim_point(50, 0.0f, 0.5f, 0.0f, 0, 0, 0),
-        anim::anim_point(100, 0.0f, 0.0f, 0.0f, 0, 0, 0)
+        //             stemp,  x,   y,  sX,  sY, angle, sprite
+        anim::anim_point(0  , 0.0f, 0.0f, 0.0f, 0, 0, 0),
+        //anim::anim_point(50 , 0.0f, 0.5f, 0.0f, 0, 0, 0),
+        anim::anim_point(100, 0.0f, 1.0f, 0.0f, 0, 0, 0)
       }
     };
   };
